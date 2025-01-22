f3a7a89dc8afb76298ff101dda2a35a1cb3bbe80
System Administrator <root@SJH-706-36.local>
2025-01-22 09:55:34 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { interval } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { DatePipe } from '@angular/common';
+import { MyserviceService } from './myservice.service';
+import { NewCmpComponent } from "./new-cmp/new-cmp.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DatePipe, CommonModule],
+  imports: [RouterOutlet, DatePipe, CommonModule, NewCmpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -29,5 +31,15 @@ export class AppComponent {
   Fruits = ["Apple", "Orange", "Grapes", "Mango", "Kiwi", "Pompagranate"];
   decimalNum1: number = 8.7589623;
   decimalNum2: number = 5.43;
+
+
+  todaydate;
+  componentproperty;
+  constructor(private myservice: MyserviceService) {
+    this.todaydate = this.myservice.showTodayDate();
+    this.componentproperty = this.myservice.serviceproperty;
   }
+}
+  
+
 
